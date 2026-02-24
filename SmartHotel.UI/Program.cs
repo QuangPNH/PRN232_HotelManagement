92f629d824cbec33d3c39325059a491240347f6f
Quang Huy <156174309+QuangPNH@users.noreply.github.com>
2026-02-24 22:36:13 +0700

    Update file version v1.0 and create project

---
+++ SmartHotel.UI/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using SmartHotel.UI.Models;
+
+namespace SmartHotel.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.Configure<ApiSettings>(
+            builder.Configuration.GetSection("ApiSettings"));
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            // Trong Program.cs
+            builder.Services.AddHttpClient("SmartHotelClient")
+                .ConfigurePrimaryHttpMessageHandler(() =>
+                {
+                    return new HttpClientHandler
+                    {
+                        ClientCertificateOptions = ClientCertificateOption.Manual,
+                        ServerCertificateCustomValidationCallback =
+                            (httpRequestMessage, cert, cetChain, policyErrors) => true
+                    };
+                }); 
+            builder.Services.AddSession();
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromHours(2);
+    });
+
+            builder.Services.AddAuthorization();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSession();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();   // 🔥 bắt buộc
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Auth}/{action=Login}/{id?}");
+
+            app.Run();
+        }
+    }
+}

22dabeee47eba3b0de25850fc9cab0c981129896
Quang Huy <156174309+QuangPNH@users.noreply.github.com>
2026-02-26 21:00:57 +0700

    Update repository: feature/Account

---
+++ WebAPI/SmartHotel.DAL/Repositories/Interface/IAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using SmartHotel.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.DAL.Repositories.Interface
+{
+    public interface IAccountRepository
+    {
+        Task<Account> GetAccountByEmailnameAsync(string email); 
+        Task<Account> GetAccountByIdAsync(int id);
+        Task<bool> IsEmailExistsAsync(string email);
+        Task CreateAccountAsync(Account account);
+        Task UpdatePasswordAsync(int accountId, string newPasswordHash); 
+        Task SaveRefreshTokenAsync(RefreshToken refreshToken);
+        Task<RefreshToken> GetRefreshTokenAsync(string token);
+        Task UpdateRefreshTokenAsync(RefreshToken refreshToken); 
+        Task RemoveOldRefreshTokensAsync(int accountId);
+    }
+}

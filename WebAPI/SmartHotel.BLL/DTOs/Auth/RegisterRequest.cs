5c2a71eb871577fb87700bfa9a8828394f78c6a0
Quang Huy <156174309+QuangPNH@users.noreply.github.com>
2026-03-05 23:40:27 +0700

    Update file AuthDTOs

---
+++ WebAPI/SmartHotel.BLL/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.DTOs.Auth
+{
+    public class RegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } // Đăng ký bằng Email
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; } // SĐT
+        public string CCCD { get; set; } // Nếu là khách thuê
+        public string Role { get; set; } = "Tenant"; // Mặc định là khách thuê
+    }
+}

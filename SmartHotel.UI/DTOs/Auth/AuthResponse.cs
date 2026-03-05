d2b8099d6905b172a5bd1231f9c12b9461a1f6ca
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2026-03-05 23:49:59 +0700

    Update code UI Authcontroller

---
+++ SmartHotel.UI/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.Auth
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiresAt { get; set; } 
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}

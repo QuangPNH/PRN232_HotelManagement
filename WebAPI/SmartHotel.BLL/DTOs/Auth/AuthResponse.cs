6316f2ffb01a7bc594663236a5fbbd33ddca2d6a
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2026-03-05 23:40:27 +0700

    Update file AuthDTOs

---
+++ WebAPI/SmartHotel.BLL/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.DTOs.Auth
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiresAt { get; set; } // Thời gian hết hạn AccessToken
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}

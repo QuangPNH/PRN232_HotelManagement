02f2696acdbdc5d3aa7d32dc6089f8e0405ff9fe
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2026-03-07 21:17:13 +0700

    Update feature/Icontractrepository

---
+++ WebAPI/SmartHotel.BLL/Services/Interface/IContractService.cs
@@ -0,0 +1,23 @@
+﻿using SmartHotel.BLL.Common;
+using SmartHotel.BLL.DTOs.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.Services.Interface
+{
+    public interface IContractService
+    {
+        Task CreateContractAsync(CreateContractRequest request);
+
+        Task TerminateContractAsync(int contractId);
+
+        Task<PagedResult<ContractResponse>> GetContractsAsync(ContractFilterRequest request);
+
+        Task<ContractResponse> GetContractByIdAsync(int id);
+
+        Task<List<ContractResponse>> GetContractsByTenantIdAsync(int tenantId);
+    }
+}

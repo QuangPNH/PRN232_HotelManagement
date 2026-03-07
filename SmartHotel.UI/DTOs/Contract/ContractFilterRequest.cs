01049d55d205d45b798ec319eef16c1ae9cb52fa
Quang Huy <156174309+QuangPNH@users.noreply.github.com>
2026-03-07 23:54:52 +0700

    Update DTO UI Contract

---
+++ SmartHotel.UI/DTOs/Contract/ContractFilterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.Contract
+{
+    public class ContractFilterRequest
+    {
+        public string? Keyword { get; set; } 
+        public bool? IsActive { get; set; }  
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ SmartHotel.UI/DTOs/Contract/CreateContractRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.Contract
+{
+    public class CreateContractRequest
+    {
+        [Required]
+        public int TenantId { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; } 
+
+        [Required]
+        public decimal DepositAmount { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; } 
+    }
+}

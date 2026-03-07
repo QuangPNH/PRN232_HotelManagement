a546f67d159d399321521e5ae8d6c664209262fe
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2026-03-07 23:54:52 +0700

    Update DTO UI Contract

---
+++ SmartHotel.UI/DTOs/Contract/ContractResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.Contract
+{
+    public class ContractResponse
+    {
+        public int ContractId { get; set; }
+
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+
+        public int TenantId { get; set; }
+        public string TenantName { get; set; }
+        public string TenantPhone { get; set; }
+
+        public decimal Price { get; set; }
+        public decimal DepositAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

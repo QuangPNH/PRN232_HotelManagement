6bfca25b1e2894dcf2739d54bde223c767855461
dhtruonghe <truongdhhe187014@fpt.edu.vn>
2026-03-10 23:42:28 +0700

    Update DTO of MeterReading

---
+++ WebAPI/SmartHotel.BLL/DTOs/MeterReading/CreateMeterReadingRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.DTOs.MeterReading
+{
+    public class CreateMeterReadingRequest
+    {
+        [Required]
+        public int ContractId { get; set; }
+
+        [Required]
+        public int ServiceId { get; set; } 
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Chỉ số mới không hợp lệ")]
+        public int NewIndex { get; set; }
+        [Required]
+        public DateTime ReadingDate { get; set; }
+    }
+}

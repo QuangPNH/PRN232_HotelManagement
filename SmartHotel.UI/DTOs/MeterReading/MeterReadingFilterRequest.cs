73cb5e58a38060fee522d7dfd01416653f9012d1
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2026-03-11 23:04:28 +0700

    Update UI DTO of MeterReading

---
+++ SmartHotel.UI/DTOs/MeterReading/MeterReadingFilterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.MeterReading
+{
+    public class MeterReadingFilterRequest
+    {
+        public int? ContractId { get; set; }
+        public int? Month { get; set; }     
+        public int? Year { get; set; }      
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ SmartHotel.UI/DTOs/MeterReading/MeterReadingResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.UI.DTOs.MeterReading
+{
+    public class MeterReadingResponse
+    {
+        public int MeterReadingId { get; set; }
+
+        public int ContractId { get; set; }
+        public string RoomNumber { get; set; } 
+        public string ServiceName { get; set; } 
+
+        public int OldIndex { get; set; }
+        public int NewIndex { get; set; }
+        public int Consumption => NewIndex - OldIndex; 
+
+        public DateTime ReadingDate { get; set; }
+    }
+}

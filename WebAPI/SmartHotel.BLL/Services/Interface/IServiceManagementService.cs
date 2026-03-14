24eb755aff610763fe465f777627abf8f8ff560f
dhtruonghe <truongdhhe187014@fpt.edu.vn>
2026-03-14 12:32:28 +0700

    Create feature Service Management Service

---
+++ WebAPI/SmartHotel.BLL/Services/Interface/IServiceManagementService.cs
@@ -0,0 +1,22 @@
+﻿using SmartHotel.BLL.Common;
+using SmartHotel.BLL.DTOs.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.Services.Interface
+{
+    public interface IServiceManagementService
+    {
+        Task<IEnumerable<ServiceResponse>> GetAllActiveServicesAsync();
+        Task<PagedResult<ServiceResponse>> GetServicesAsync(string? keyword, int pageIndex, int pageSize);
+        Task CreateServiceAsync(CreateServiceRequest request);
+        Task UpdateServiceAsync(int id, CreateServiceRequest request);
+        Task DeleteServiceAsync(int id);
+
+        Task<ServiceResponse> GetServiceByIdAsync(int id);
+
+    }
+}

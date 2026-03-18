2a471aac9d42485127049b7b774cd2996bbf81de
dhtruonghe <truongdhhe187014@fpt.edu.vn>
2026-03-18 21:40:23 +0700

    Create RoomStatus.cs

---
+++ WebAPI/SmartHotel.BLL/Common/RoomStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.BLL.Common
+{
+    public static class RoomStatus
+    {
+        public const string Available = "Available";  
+        public const string Occupied = "Occupied";    
+        public const string Maintenance = "Maintenance"; 
+        public const string Cleaning = "Cleaning";     
+    }
+}

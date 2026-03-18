44e25d8fa633a23aec8c8b3d17a0921430ac6425
dhtruonghe <truongdhhe187014@fpt.edu.vn>
2026-03-18 22:52:23 +0700

    Create DashboardViewModel.cs

---
+++ SmartHotel.UI/DTOs/Statistics/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartHotel.UI.DTOs.Statistics
+{
+    public class DashboardViewModel
+    {
+        // Hứng API Revenue (List các tháng)
+        public List<RevenueStatisticResponse> RevenueData { get; set; } = new List<RevenueStatisticResponse>();
+
+        // Hứng API Occupancy (Trạng thái phòng)
+        public RoomStatusStatisticResponse RoomStatus { get; set; } = new RoomStatusStatisticResponse();
+    }
+}
